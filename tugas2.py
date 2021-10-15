4bdeba0fe83151d838360ebdf5da7f098c70f2aa
Yanda29 <91973633+Yanda29@users.noreply.github.com>
2021-10-15 23:06:26 +0700

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -1,37 +1,42 @@
-def main():
+aku=1500
+while aku ==1500:
+    import sys
+    
+    elo= {"jb","lw","lw"}
 
     print("jb-Jabodetabek\ndw-Pulau Jawa\nlw-Luar Jawa")
     destinasi= input("Tujuan Barang(q-quit)= ")
-
     if destinasi=="q":
-                    exit(0)
-    beban = int(input("Berat Barang = "))
+                    sys.exit(0)
     if destinasi == "jb":
-            harga= 10000
+            price= 10000
     elif destinasi == "dw":
-            harga= 25000
+            price= 25000
     elif destinasi == "lw":
-            harga=50000
+            price=50000
     else:
-            print ("Invalid input")
-            destinasi= input("tujuan barang= ")
+        print ("Invalid input")
+        destinasi= input("Tujuan Barang(q-quit)= ")
+    
+    
+    jb =price= 10000
+    dw =price= 25000
+    lw =price= 50000
+    beban = int(input("Berat Barang = "))
             
     if beban <= 20:
             n = 15000
     elif beban >= 20:
-            n = beban * 1500
+            n = (15000 + ((beban - 20) *1500))
+    else:
+        print ("Invalid input")
+        beban = int(input("Berat Barang = "))
 
-    ppn = (harga+n)/10
+    ppn = int(price+n)/10
 
-    print("------------Rincian Biaya-----------")
-    print("1.tujuan= ",destinasi,     "harga= Rp.",harga)
-    print("2.Berat=  ",beban,"kg", "harga= Rp.",n)
-    print("3.PPN 10%= Rp.", ppn)
-    print("4.Total= Rp.",harga+n+ppn)
-    print("-------------------------------------")
-    main()
-
-
-main()
-
-
+    print('====================total================================\n'
+    f'tujuan= ,destinasi,     harga= Rp.{price}\n'
+    f'Berat=  {beban}kg ,harga= Rp.,{n}\n'
+    f'PPN 10%= Rp., {ppn}\n'
+    f'Total= Rp.",{price+n+ppn}')
+    print("==========================================================")
